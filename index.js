0cc8095d070c505e3b02b2db4036fb9acab6a873
James_Matara <jamesmatara11@gmail.com>
2024-03-31 11:27:15 +0300

    updated functionality

--- index.js
+++ index.js
@@ -13,9 +13,27 @@ function generatePassword(
   const symbolChars = "!@#$%&+-?"
 
   let allowedChars = "";
+  let password = "";
+
+  allowedChars += includeLowerCase ? LowerCaseChars : "";
+  allowedChars += includeUpperCase ? UpperCaseChars : "";
+  allowedChars += includeNumbers ? numberChars : "";
+  allowedChars += includeSymbols ? symbolChars : "";
+
+  if (length <= 0) {
+    return `(Password length must be at least 1)`;
+  }
+  if (allowedChars.length === 0) {
+    return `(at least one set of characters needs to be selected)`;
+  }
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars[randomIndex];
+  }
   
 
-  return "";
+  return password;
 }
 
 const passwordLength = 12;
